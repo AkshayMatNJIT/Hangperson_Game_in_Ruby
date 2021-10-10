e5f341192d152edd4c01b3b45eb84aebc13d1baa
Akshay Mutha NJIT <arm8@njit.edu>
2021-10-10 16:32:52 -0400

    third commit

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 ruby '>= 2.2.0', '< 3.0.0'
 gem 'sinatra', '>= 2.0.1'
+
+group :development do
+gem 'rerun'
+end
--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 class MyApp < Sinatra::Base
 get '/' do
-"<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+"<!DOCTYPE html><html><head></head><body><h1>Bye World</h1></body></html>"
 end
 end
